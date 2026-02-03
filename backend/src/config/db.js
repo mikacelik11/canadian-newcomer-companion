31b78ab4ee747972633eae5ce2682b59a4d922a7
MikaC11-bit <mikael_celik@hotmail.com>
2026-02-02 19:23:28 -0800

    removed file

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient(); //
 
 // Test database connection
 async function testConnection() {
