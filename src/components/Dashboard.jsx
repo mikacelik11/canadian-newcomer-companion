84f33f102cd3b1f16fdca11f363ff22d270ac1f3
MikaC11-bit <mikael_celik@hotmail.com>
2026-02-11 12:54:27 -0800

    quick doc

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; // usestate and useffect import 
 import Navigation from './Navigation';
 import { clearAllData, getTaskProgress } from '../utils/storage';
 
